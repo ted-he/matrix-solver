5eab197aba6d6f17a4bfe2ed1585afd66c0ee0b7
Ted He <ted.he04@gmail.com>
2022-10-16 18:14:13 -0400

    v1.0

--- SoESolver.java
+++ SoESolver.java
@@ -16,44 +16,43 @@ public class SoESolver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         /*
-        final double[][] oCoeffs = {
-            {2, 4, -3},
-            {-6, 2, 7},
-            {4, -3, 2}
-        };
-        
-        final double[] oConsts = {-3, 39, 9};
-        
-        final double[][] coeffs = {
-            {2, 4, -3},
-            {-6, 2, 7},
-            {4, -3, 2}
-        };
-        
-        final double[] consts = {-3, 39, 9};
-        */
-        
+         * final double[][] oCoeffs = {
+         * {2, 4, -3},
+         * {-6, 2, 7},
+         * {4, -3, 2}
+         * };
+         * 
+         * final double[] oConsts = {-3, 39, 9};
+         * 
+         * final double[][] coeffs = {
+         * {2, 4, -3},
+         * {-6, 2, 7},
+         * {4, -3, 2}
+         * };
+         * 
+         * final double[] consts = {-3, 39, 9};
+         */
+
         double[][] oCoeffs = {
-            {2, 2, -1, 1},
-            {4, 0, -1, 2},
-            {8, 5, -3, 4},
-            {3, 3, -2, 2}
+                { 2, 2, -1, 1 },
+                { 4, 0, -1, 2 },
+                { 8, 5, -3, 4 },
+                { 3, 3, -2, 2 }
         };
-        
-        double[] oConsts = {4, 6, 12, 6};
-        
+
+        double[] oConsts = { 4, 6, 12, 6 };
+
         double[][] coeffs = {
-            {2, 2, -1, 1},
-            {4, 0, -1, 2},
-            {8, 5, -3, 4},
-            {3, 3, -2, 2}
+                { 2, 2, -1, 1 },
+                { 4, 0, -1, 2 },
+                { 8, 5, -3, 4 },
+                { 3, 3, -2, 2 }
         };
-        
-        double[] consts = {4, 6, 12, 6};
-        
-        
+
+        double[] consts = { 4, 6, 12, 6 };
+
         System.out.println("TBS:");
         display(coeffs, consts);
 
@@ -63,8 +62,8 @@ public class SoESolver {
         display(REFMtx[0], REFMtx[1][0]);
 
         double[] solution = solve(REFMtx[0], consts);
-        
-        if(checkSol(oCoeffs, oConsts, solution)) {
+
+        if (checkSol(oCoeffs, oConsts, solution)) {
             System.out.print("\nVALID SOLUTION FOUND: ");
             display(solution);
         } else {
@@ -90,19 +89,19 @@ public class SoESolver {
         System.out.println("");
 
     }
-    
+
     public static void display(double[][] mtx) {
-        for(double[] row : mtx) {
-            for(double col : row) {
+        for (double[] row : mtx) {
+            for (double col : row) {
                 System.out.print(col + "\t");
             }
             System.out.println("");
         }
     }
-    
+
     public static void display(double[] array) {
         System.out.print("[");
-        for(double element : array) {
+        for (double element : array) {
             System.out.print(element + " ");
         }
         System.out.println("\b]\n");
@@ -130,7 +129,7 @@ public class SoESolver {
 
         }
 
-        double[][][] output = {coeffs, {consts}};
+        double[][][] output = { coeffs, { consts } };
 
         return output;
 
@@ -157,33 +156,34 @@ public class SoESolver {
         return sol;
 
     }
-    
+
     public static boolean checkSol(double[][] coeffs, double[] consts, double[] solution) {
-        
+
         System.out.println("Solution found. Verifying...");
         System.out.println("System Matrix: ");
         display(coeffs, consts);
         System.out.print("Solution: ");
         display(solution);
-        
-        for(int row = 0; row < coeffs.length; row++) {
-            
+
+        for (int row = 0; row < coeffs.length; row++) {
+
             double curSum = 0;
             double[] curRow = coeffs[row];
-            
+
             System.out.print("Checking equation " + (row + 1) + ": ");
-            
-            for(int col = 0; col < coeffs.length; col++) {
+
+            for (int col = 0; col < coeffs.length; col++) {
                 curSum += (curRow[col] * solution[col]);
                 System.out.print(curRow[col] + "(" + solution[col] + ") + ");
             }
-            
+
             System.out.println("\b\b= " + consts[row]);
-             
-            if (consts[row] != curSum) return false;
-            
+
+            if (consts[row] != curSum)
+                return false;
+
         }
-        
+
         return true;
     }
 
